642c2e6918a32c1e6f5e5a76650ea9f1dbdf47b7
Taehwan Kim <49132911+taehwan642@users.noreply.github.com>
2022-04-22 20:51:40 +0900

    LaserScript Enum 추가, 내부 구상 중

--- Engine/LaserScript.cpp
+++ Engine/LaserScript.cpp
@@ -1,5 +1,18 @@
 #include "pch.h"
 #include "LaserScript.h"
+#include "GameObject.h"
+#include "MeshRenderer.h"
+#include "Material.h"
+#include "Timer.h"
+#include "TextureAnimator.h"
+#include "Input.h"
+
+void LaserScript::Init()
+{
+	// 애니메이션 불러오기 d
+	_animator->LoadAnimation("LaserTextures");
+	_animator->SetLoop(false);
+}
 
 LaserScript::~LaserScript()
 {
@@ -7,4 +20,40 @@ LaserScript::~LaserScript()
 
 void LaserScript::LateUpdate()
 {
+	if(_init == false)
+	{
+		_init = true;
+		Init();
+	}
+	if (INPUT->GetButtonDown(KEY_TYPE::KEY_1))
+	{
+		_laserState = (_laserState == LaserState::LOCKON) ? LaserState::FIRE : LaserState::LOCKON;
+	}
+
+	// 만약 Lock 상태일 때?
+	if (_laserState == LaserState::LOCKON)
+	{
+		// 그에 따른 텍스처 넘겨주기
+		GetMeshRenderer()->GetMaterial()->SetTexture(0, _animator->GetTexture(0));
+		_scrollSpeed = 1.f;
+	}
+	// 만약 Fire 상태일 때?
+	else if (_laserState == LaserState::FIRE)
+	{
+		GetMeshRenderer()->GetMaterial()->SetTexture(0, _animator->GetTexture(1));
+		_scrollSpeed = 2.f;
+	}
+	// 언제 끝나나?
+	// End는 언제?
+	else if (_laserState == LaserState::END)
+	{
+
+	}
+	
+	// state에 따른 uv 스크롤 속도 변환
+	// 충돌처리 
+
+	static float inputTime = 0;
+	inputTime += DELTA_TIME;
+	GetMeshRenderer()->GetMaterial()->SetFloat(0, inputTime * _scrollSpeed);
 }
--- Engine/LaserScript.h
+++ Engine/LaserScript.h
@@ -1,11 +1,21 @@
 #pragma once
 #include "MonoBehaviour.h"
+
+enum class LaserState
+{
+    LOCKON,
+    FIRE,
+    END
+};
+
+class TextureAnimator;
 class LaserScript :
     public MonoBehaviour
 {
 public:
     MONOBEHAVIOUR(LaserScript)
 
+    void Init();
     virtual ~LaserScript();
 
     virtual void OnCollisionEnter(std::shared_ptr<class BaseCollider> collider) override {}
@@ -13,6 +23,17 @@ public:
     virtual void OnCollisionExit(std::shared_ptr<class BaseCollider> collider) override {}
 
     virtual void LateUpdate() override;
+
+    void SetFireTime(float time) { _fireTime = time; };
+
+private:
+    LaserState _laserState = LaserState::LOCKON;
+    float _fireTime = 10.0f;
+    float _scrollSpeed = 1.f;
+    
+    bool _init = false;
+
+    std::shared_ptr<TextureAnimator> _animator;
 
     RTTR_ENABLE()
     RTTR_REGISTRATION_FRIEND
