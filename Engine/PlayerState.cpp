8db8fcc8f0f568d7b4902c509f5da7200a8ebed4
taehwan642 <taehwan642@naver.com>
2022-05-03 19:18:12 +0900

    Player State 구현 완료

--- Engine/PlayerState.cpp
+++ Engine/PlayerState.cpp
@@ -42,10 +42,16 @@ int FlightIdleState::handleInput()
 			std::static_pointer_cast<NierAnimator>(object->GetAnimator())->SetAnimationIndex(5);
 		}
 	}
+
 	if (INPUT->GetButtonDown(KEY_TYPE::SPACE))
 	{
+		_object.lock()->GetAudioSource()->LoadAudio(L"..\\Resources\\Audio\\player\\pl0010_21.wav");
+		_object.lock()->GetAudioSource()->SetLoop(false);
+		_object.lock()->GetAudioSource()->SetVolume(0.2f);
+		_object.lock()->GetAudioSource()->Play();
 		return FLIGHT_TO_COMBAT;//_object.lock()->GetComponent<Player>()->ChangeFlightCombatMode();
 	}
+
 	if (INPUT->GetButton(KEY_TYPE::E))
 	{
 		_object.lock()->GetComponent<Player>()->Attack();
@@ -57,19 +63,43 @@ int FlightIdleState::handleInput()
 int CombatIdleState::handleInput()
 {
 	_object.lock()->GetComponent<Player>()->Move();
-	for (int i = 0; i < _object.lock()->GetTransform()->GetChild(0)->GetChildCount(); ++i)
-	{
-		std::shared_ptr<GameObject> object = _object.lock()->GetTransform()->GetChild(0)->GetChild(i)->GetGameObject();
-		std::static_pointer_cast<NierAnimator>(object->GetAnimator())->SetAnimationIndex(7);
-	}
+	
 	if (INPUT->GetButtonDown(KEY_TYPE::SPACE))
 	{
+		_object.lock()->GetAudioSource()->LoadAudio(L"..\\Resources\\Audio\\player\\pl0010_21.wav");
+		_object.lock()->GetAudioSource()->SetLoop(false);
+		_object.lock()->GetAudioSource()->SetVolume(0.2f);
+		_object.lock()->GetAudioSource()->Play();
 		return COMBAT_TO_FLIGHT;//_object.lock()->GetComponent<Player>()->ChangeFlightCombatMode();
 	}
+
 	if (INPUT->GetButton(KEY_TYPE::E))
 	{
 		_object.lock()->GetComponent<Player>()->Attack();
+		for (int i = 0; i < _object.lock()->GetTransform()->GetChild(0)->GetChildCount(); ++i)
+		{
+			std::shared_ptr<GameObject> object = _object.lock()->GetTransform()->GetChild(0)->GetChild(i)->GetGameObject();
+			std::shared_ptr<NierAnimator> anim = std::static_pointer_cast<NierAnimator>(object->GetAnimator());
+			anim->SetAnimationIndex(8);
+		}
 		return COMBAT_FIRE;
+	}
+	else
+	{
+		for (int i = 0; i < _object.lock()->GetTransform()->GetChild(0)->GetChildCount(); ++i)
+		{
+			std::shared_ptr<GameObject> object = _object.lock()->GetTransform()->GetChild(0)->GetChild(i)->GetGameObject();
+			std::static_pointer_cast<NierAnimator>(object->GetAnimator())->SetAnimationIndex(7);
+		}
+	}
+
+	if (INPUT->GetButtonDown(KEY_TYPE::C))
+	{
+		_object.lock()->GetAudioSource()->LoadAudio(L"..\\Resources\\Audio\\player\\pl0010_8.wav");
+		_object.lock()->GetAudioSource()->SetLoop(false);
+		_object.lock()->GetAudioSource()->SetVolume(0.2f);
+		_object.lock()->GetAudioSource()->Play();
+		return COMBAT_ATTACK1;
 	}
 	return COMBAT_IDLE;
 }
@@ -77,7 +107,6 @@ int CombatIdleState::handleInput()
 int FlightFireState::handleInput()
 {
 	_object.lock()->GetComponent<Player>()->Move();
-	
 	return FLIGHT_IDLE;
 }
 
@@ -90,12 +119,69 @@ int CombatFireState::handleInput()
 int CombatAttack1State::handleInput()
 {
 	_object.lock()->GetComponent<Player>()->Move();
+	for (int i = 0; i < _object.lock()->GetTransform()->GetChild(0)->GetChildCount(); ++i)
+	{
+		std::shared_ptr<GameObject> object = _object.lock()->GetTransform()->GetChild(0)->GetChild(i)->GetGameObject();
+		std::shared_ptr<NierAnimator> anim = std::static_pointer_cast<NierAnimator>(object->GetAnimator());
+		anim->SetAnimationIndex(2);
+
+		if (anim->GetCurrentFrame() > 340)
+		{
+			if (INPUT->GetButtonDown(KEY_TYPE::C))
+			{
+				_object.lock()->GetAudioSource()->LoadAudio(L"..\\Resources\\Audio\\player\\pl0010_8.wav");
+				_object.lock()->GetAudioSource()->SetLoop(false);
+				_object.lock()->GetAudioSource()->SetVolume(0.2f);
+				_object.lock()->GetAudioSource()->Play();
+				return COMBAT_ATTACK2;
+			}
+
+			if (INPUT->GetButton(KEY_TYPE::E))
+			{
+				return COMBAT_IDLE;
+			}
+		}
+
+		if (anim->GetAnimationEnd())
+		{
+			return COMBAT_IDLE;
+		}
+	}
+
 	return COMBAT_ATTACK1;
 }
 
 int CombatAttack2State::handleInput()
 {
 	_object.lock()->GetComponent<Player>()->Move();
+	for (int i = 0; i < _object.lock()->GetTransform()->GetChild(0)->GetChildCount(); ++i)
+	{
+		std::shared_ptr<GameObject> object = _object.lock()->GetTransform()->GetChild(0)->GetChild(i)->GetGameObject();
+		std::shared_ptr<NierAnimator> anim = std::static_pointer_cast<NierAnimator>(object->GetAnimator());
+		anim->SetAnimationIndex(3);
+
+		if (anim->GetCurrentFrame() > 440)
+		{
+			if (INPUT->GetButtonDown(KEY_TYPE::C))
+			{
+				_object.lock()->GetAudioSource()->LoadAudio(L"..\\Resources\\Audio\\player\\pl0010_8.wav");
+				_object.lock()->GetAudioSource()->SetLoop(false);
+				_object.lock()->GetAudioSource()->SetVolume(0.2f);
+				_object.lock()->GetAudioSource()->Play();
+				return COMBAT_ATTACK1;
+			}
+
+			if (INPUT->GetButton(KEY_TYPE::E))
+			{
+				return COMBAT_IDLE;
+			}
+		}
+
+		if (anim->GetAnimationEnd())
+		{
+			return COMBAT_IDLE;
+		}
+	}
 	return COMBAT_ATTACK2;
 }
 
@@ -109,9 +195,10 @@ int FlightToCombatState::handleInput()
 			continue;
 	
 		std::shared_ptr<GameObject> object = childTransform->GetGameObject();
-		std::static_pointer_cast<NierAnimator>(object->GetAnimator())->SetAnimationIndex(1);
+		std::shared_ptr<NierAnimator> anim = std::static_pointer_cast<NierAnimator>(object->GetAnimator());
+		anim->SetAnimationIndex(1);
 
-		if (std::static_pointer_cast<NierAnimator>(object->GetAnimator())->GetAnimationEnd())
+		if (anim->GetAnimationEnd())
 		{
 			ADDLOG("FLIGHT TO COMBAT END\n");
 			return COMBAT_IDLE;
@@ -130,9 +217,10 @@ int CombatToFlightState::handleInput()
 			continue;
 		
 		std::shared_ptr<GameObject> object = childTransform->GetGameObject();
-		std::static_pointer_cast<NierAnimator>(object->GetAnimator())->SetAnimationIndex(0);
+		std::shared_ptr<NierAnimator> anim = std::static_pointer_cast<NierAnimator>(object->GetAnimator());
+		anim->SetAnimationIndex(0);
 
-		if (std::static_pointer_cast<NierAnimator>(object->GetAnimator())->GetAnimationEnd())
+		if (anim->GetAnimationEnd())
 		{
 			ADDLOG("COMBAT TO FLIGHT END\n");
 			return FLIGHT_IDLE;
@@ -144,6 +232,17 @@ int CombatToFlightState::handleInput()
 int FlightDodgeState::handleInput()
 {
 	_object.lock()->GetComponent<Player>()->Move();
+	for (int i = 0; i < _object.lock()->GetTransform()->GetChild(0)->GetChildCount(); ++i)
+	{
+		std::shared_ptr<GameObject> object = _object.lock()->GetTransform()->GetChild(0)->GetChild(i)->GetGameObject();
+		std::shared_ptr<NierAnimator> anim = std::static_pointer_cast<NierAnimator>(object->GetAnimator());
+		anim->SetAnimationIndex(4);
+
+		if (anim->GetAnimationEnd())
+		{
+			return FLIGHT_IDLE;
+		}
+	}
 	return FLIGHT_DODGE;
 }
 
@@ -152,9 +251,10 @@ int FlightDeadState::handleInput()
 	for (int i = 0; i < _object.lock()->GetTransform()->GetChild(0)->GetChildCount(); ++i)
 	{
 		std::shared_ptr<GameObject> object = _object.lock()->GetTransform()->GetChild(0)->GetChild(i)->GetGameObject();
-		std::static_pointer_cast<NierAnimator>(object->GetAnimator())->SetAnimationIndex(9);
+		std::shared_ptr<NierAnimator> anim = std::static_pointer_cast<NierAnimator>(object->GetAnimator());
+		anim->SetAnimationIndex(9);
 
-		if (std::static_pointer_cast<NierAnimator>(object->GetAnimator())->GetAnimationEnd())
+		if (anim->GetAnimationEnd())
 		{
 			_object.lock()->SetActive(false);
 		}
