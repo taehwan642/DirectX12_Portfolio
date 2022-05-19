ad6e61ab560baff850905e9df75259078db7d35f
taehwan642 <taehwan642@naver.com>
2022-05-19 23:25:55 +0900

    EnemyShooting 구상 중

--- Engine/EnemyShooting.cpp
+++ Engine/EnemyShooting.cpp
@@ -1,2 +1,18 @@
 #include "pch.h"
 #include "EnemyShooting.h"
+
+void EnemyLaserShooting::Shoot()
+{
+}
+
+void EnemyRapidShooting::Shoot()
+{
+}
+
+void EnemyHeavyShooting::Shoot()
+{
+}
+
+void EnemyThreeWayShooting::Shoot()
+{
+}
--- Engine/EnemyShooting.h
+++ Engine/EnemyShooting.h
@@ -1,5 +1,30 @@
 #pragma once
 class EnemyShooting
 {
+public:
+	virtual void Shoot() = 0;
 };
 
+class EnemyLaserShooting : public EnemyShooting
+{
+public:
+	virtual void Shoot() override;
+};
+
+class EnemyRapidShooting : public EnemyShooting
+{
+public:
+	virtual void Shoot() override;
+};
+
+class EnemyHeavyShooting : public EnemyShooting
+{
+public:
+	virtual void Shoot() override;
+};
+
+class EnemyThreeWayShooting : public EnemyShooting
+{
+public:
+	virtual void Shoot() override;
+};
