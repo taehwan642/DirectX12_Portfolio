ade603b3edf99845b7b00ea1b3f4fb7b30b9be14
taehwan642 <taehwan642@naver.com>
2022-10-29 00:16:35 +0900

    lua 이식 성공

--- ScriptLanguageTest/ScriptLanguageTest/main.cpp
+++ ScriptLanguageTest/ScriptLanguageTest/main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <string>
+#include <memory>
+
+extern "C"
+{
+#include "lua/include/lua.h"
+#include "lua/include/lauxlib.h"
+#include "lua/include/lualib.h"
+}
+
+#pragma comment(lib, "lua/liblua54.a")
 
 int main()
 {
+	lua_State* L = luaL_newstate();
+
+	int r = luaL_dofile(L, "testScript.lua");
+
+	if (r == LUA_OK)
+	{
+		lua_getglobal(L, "a");
+		if (lua_isnumber(L, -1))
+		{
+			float a = (float)lua_tonumber(L, -1);
+			std::cout << a << std::endl;
+		}
+
+	}
+	else
+	{
+		std::string errormsg = lua_tostring(L, -1);
+		std::cout << errormsg << std::endl;
+	}
+
 	return 0;
 }
