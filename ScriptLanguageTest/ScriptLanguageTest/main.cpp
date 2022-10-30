205ff50fc1e0bb95fc5844dd5d11fab0e4e325fd
taehwan642 <taehwan642@naver.com>
2022-10-30 21:25:38 +0900

    함수 연결 테스트 통과

--- ScriptLanguageTest/ScriptLanguageTest/main.cpp
+++ ScriptLanguageTest/ScriptLanguageTest/main.cpp
@@ -11,26 +11,63 @@ extern "C"
 
 #pragma comment(lib, "lua/liblua54.a")
 
-int main()
+bool CheckLua(lua_State* L, int r)
 {
-	lua_State* L = luaL_newstate();
-
-	int r = luaL_dofile(L, "testScript.lua");
-
-	if (r == LUA_OK)
-	{
-		lua_getglobal(L, "a");
-		if (lua_isnumber(L, -1))
-		{
-			float a = (float)lua_tonumber(L, -1);
-			std::cout << a << std::endl;
-		}
-
-	}
-	else
+	if (r != LUA_OK)
 	{
 		std::string errormsg = lua_tostring(L, -1);
 		std::cout << errormsg << std::endl;
+		return false;
+	}
+	return true;
+}
+
+int main()
+{
+	lua_State* L = luaL_newstate();
+	luaL_openlibs(L);
+
+	//if (r == LUA_OK)
+	//{
+	//	lua_getglobal(L, "a");
+	//	if (lua_isnumber(L, -1))
+	//	{
+	//		float a = (float)lua_tonumber(L, -1);
+	//		std::cout << a << std::endl;
+	//	}
+	//
+	//}
+	//else
+	//{
+	//	std::string errormsg = lua_tostring(L, -1);
+	//	std::cout << errormsg << std::endl;
+	//}
+
+	if (CheckLua(L, luaL_dofile(L, "testScript.lua")))
+	{
+		for (int i = 0; i < 30; ++i)
+		{
+			lua_getglobal(L, "Update");
+			if (lua_isfunction(L, -1))
+			{
+				lua_pushnumber(L, 0.1);
+				if (CheckLua(L, lua_pcall(L, 1, 1, 0)))
+				{
+					if (lua_istable(L, -1))
+					{
+						lua_pushstring(L, "x");
+						lua_gettable(L, -2);
+						std::cout << lua_tostring(L, -1) << std::endl;
+						lua_pop(L, 1);
+						lua_pushstring(L, "y");
+						lua_gettable(L, -2);
+						std::cout << lua_tostring(L, -1) << std::endl;
+						lua_pop(L, 1);
+					}
+
+				}
+			}
+		}
 	}
 
 	return 0;
--- ScriptLanguageTest/ScriptLanguageTest/testScript.lua
+++ ScriptLanguageTest/ScriptLanguageTest/testScript.lua
@@ -1,2 +1,12 @@
-a = 7 + 11
-a = a + 100
+-- a = 30
+
+transform = {}
+transform["x"] = 0
+transform["y"] = 0
+
+function Update(elapsedTime)
+	print("elapsedTime : ("..elapsedTime..")")
+	transform["x"] = transform["x"] + 10
+	transform["y"] = transform["y"] + 5
+	return transform
+end
